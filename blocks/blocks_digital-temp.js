c3e25a3173a238360413839cc0a4ac18851c223e
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 15:43:43 +0700

    add generator

--- blocks/blocks_digital-temp.js
+++ blocks/blocks_digital-temp.js
@@ -1,18 +1,32 @@
-Blockly.Blocks['ky_028_temp_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-028 TEMP");
-    this.appendValueInput("KY_028_TEMP_ANALOG_PIN")
-        .setCheck("Number")
-        .appendField("ANALOG PIN");
-    this.appendValueInput("KY_028_TEMP_DIGITAL_PIN")
-        .setCheck("Number")
-        .appendField("DIGITAL PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_028_digital_temp_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY_028_DIGITAL_TEMP");
+      this.appendDummyInput()
+        .appendField("ANALOG PIN")
+        .appendField(
+          new Blockly.FieldTextInput("A0"),
+          "KY_028_DIGITAL_TEMP_ANALOG_PIN"
+        )
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_028_DIGITAL_TEMP_ANALOG_VALUE"),
+          "KY_028_DIGITAL_TEMP_ANALOG_VALUE"
+        );
+      this.appendDummyInput()
+        .appendField("DIGITAL PIN")
+        .appendField(
+          new Blockly.FieldTextInput("0"),
+          "KY_028_DIGITAL_TEMP_DIGITAL_PIN"
+        )
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_028_DIGITAL_TEMP_DIGITAL_VALUE"),
+          "KY_028_DIGITAL_TEMP_DIGITAL_VALUE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_digital-temp.js
+++ blocks/generators_digital-temp.js
@@ -1,7 +1,39 @@
-Blockly.JavaScript['ky_028_temp_block'] = function(block) {
-  var value_ky_028_temp_analog_pin = Blockly.JavaScript.valueToCode(block, 'KY_028_TEMP_ANALOG_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_ky_028_temp_digital_pin = Blockly.JavaScript.valueToCode(block, 'KY_028_TEMP_DIGITAL_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
+module.exports = function(Blockly) {
+  Blockly.JavaScript["ky_028_digital_temp_block"] = function(block) {
+    var text_ky_028_digital_temp_analog_pin = block.getFieldValue(
+      "KY_028_DIGITAL_TEMP_ANALOG_PIN"
+    );
+    var variable_ky_028_digital_temp_analog_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_028_DIGITAL_TEMP_ANALOG_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    var text_ky_028_digital_temp_digital_pin = block.getFieldValue(
+      "KY_028_DIGITAL_TEMP_DIGITAL_PIN"
+    );
+    var variable_ky_028_digital_temp_digital_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_028_DIGITAL_TEMP_DIGITAL_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	
+	#VARIABLE
+	int KY_028_DIGITAL_TEMP_DIGITAL_PIN = ${text_ky_028_digital_temp_digital_pin}; // KY-028 digital interface
+	int KY_028_DIGITAL_TEMP_ANALOG_PIN = ${text_ky_028_digital_temp_analog_pin}; // KY-028 analog interface
+	#END
+	
+	#SETUP
+	  pinMode(KY_028_DIGITAL_TEMP_DIGITAL_PIN, INPUT);
+	  pinMode(KY_028_DIGITAL_TEMP_ANALOG_PIN, OUTPUT);
+	  Serial.begin(9600);
+	#END
+
+	// Read the digital interface
+  	${variable_ky_028_digital_temp_digital_value} = digitalRead(KY_028_DIGITAL_TEMP_DIGITAL_PIN); 
+  	// Read the analog interface
+  	${variable_ky_028_digital_temp_analog_value} = analogRead(KY_028_DIGITAL_TEMP_ANALOG_PIN); 
+  	delay(100);
+    `;
+    return code;
+  };
 };
